4af3bb377ec9f675d46447f8d82b4ed3c5e396f9
Subbiksa Shanmughasundaram <ssubbiksaa@gmail.com>
2018-03-12 00:08:08 +1100

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package mini_net;
+
+/**
+ * @author ssubb
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   System.out.println("MiniNet Menu");
+		   System.out.println("!================!");
+		   System.out.println("1.Add a new profile");
+		   System.out.println("2.Display every profile");
+		   System.out.println("3.Make a selection");
+		   System.out.println("4.Are they direct friends?");
+		   System.out.println("5.Update the profile information");
+		   System.out.println("6.Delete the profile");
+		   System.out.println("7.Make a new connection");
+		   System.out.println("8.List the parents");
+           int choice = 0;
+           switch(choice) {
+           case 1:
+        	   
+              break;
+           case 2:
+        	   
+               break;
+           }
+	}
+
+}
---
+++ MiniNet.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mini_net;
+
+/**
+ * @author ssubb
+ *
+ */
+public class MiniNet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
