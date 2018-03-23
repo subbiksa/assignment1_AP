5a9d59b5ac7fadfc762101f70aa86f75b9e22034
RICHA RAWAT <rawricha841@gmail.com>
2018-03-23 22:54:43 +1100

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,23 +1,22 @@
-package Mininet;
+ package Mininet;
 
 import java.util.ArrayList;
 
-//author subbiksa
+public class Profile {//creation of new class Profile
 
-public class Profile{//creation of new class Profile(super class)
-
-	 String profileName;//variable declared for each profile
+	 String name;//variable declared for each profile
 	 int age;
 	 String status;
 	 String image;
 	 String friend1;
-	 String friend2;
-	public Profile(String profileName) { //constructor declaration
-		
-		this.profileName=profileName;
+	 String friend2; String friend3;
+	public String getName(String profileName) {  
+		name = profileName;
+		return name;
 	}
-	public void getAge(int profileAge) {//assigning age 
+	public int getAge(int profileAge) {//assigning age 
 		age= profileAge;
+		return age;
 	}
 	public void getStatus(String profileStatus) {//assign status
 		status= profileStatus;
@@ -25,34 +24,40 @@ public class Profile{//creation of new class Profile(super class)
 	public void getImage(String profileImage) {//assign image
 		image= profileImage;
 	}
-	public void getfriend1(String profileFriend1) {//assign friend1
+	public void getFriend1(String profileFriend1) {//assign friend1
 		friend1= profileFriend1;
 	}
 	public void getFriend2(String profileFriend2) {//assign friend2
 		friend2= profileFriend2;
 	}
+	public void getFriend3(String profileFriend3) {//assign friend3
+		friend3 = profileFriend3;
+	}
 	public void displayProfile() {//print the details of the profile
-		System.out.println("Name:"+profileName);
+		System.out.println("Name:"+name);
 		System.out.println("Age:"+age);
 		System.out.println("Status:"+status);
 		System.out.println("Image:"+image);
-		System.out.println("List of Friends\n"+friend1+"\n"+friend2);
+		System.out.println("List of Friends:"+friend1+"\n"+friend2+"\n"+ friend3);
+		
 	}
-    public void deleteProfile() {//method to delete the profile
+	public void deleteProfile() {
     	ArrayList<String> lis = new ArrayList<String>(5);
     	lis.add("Chandler Bing");
     	lis.add("Joey Tribbiani");
     	lis.add("Phoebe Buffay");
     	lis.add("Rachel Greene");
-    	lis.add("Monica Geller");//add to the list
+    	lis.add("Monica Geller");
     	System.out.println("List of Profiles");
-    	for(String value:lis) {
-    		System.out.println(" "+value);//display the list of values
-    	}
-    	lis.remove(0);//remove the value at arg 0
-    	System.out.println("New profile list");
     	for(String value:lis) {
     		System.out.println(" "+value);
     	}
-    }
+    	lis.remove(0);
+    	System.out.println("New profile list");
+    	for(String value:lis) {
+    		System.out.println(" "+value);
+}
+	}
+    	
+
 }
